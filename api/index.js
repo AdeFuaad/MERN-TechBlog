229f164f0e0eff4da02635ae2da8c1633e922177
Fuaad Shobambi <113994853+AdeFuaad@users.noreply.github.com>
2023-10-12 23:15:39 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require("fs");
 const Post = require("./models/Post");
 const postModel = require("./models/Post");
 
-app.use(cors({ credentials: true, origin: "https://techblogaf.netlify.app/" }));
+app.use(cors({ credentials: true, origin: "https://6528b5d9226bfb58b15cb092--capable-custard-f125c8.netlify.app" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
